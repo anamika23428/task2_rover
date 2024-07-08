34364c87d0852cd8fbe0ec36b59c7a92bd54ce78
anamika23428 <anamika23428@gmail.com>
2024-07-08 23:48:07 +0530

    add src folder to repo

---
+++ src/task2_pack/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/anamika/task2_ws/src/task2_pack/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
